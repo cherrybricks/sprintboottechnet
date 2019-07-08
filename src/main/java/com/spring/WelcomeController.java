94d32f6fafe25357b520529209863209b08b3e0a
cherrybricks <48308181+cherrybricks@users.noreply.github.com>
2019-07-09 01:09:34 +0800

    Add files via upload

---
+++ src/main/java/com/spring/WelcomeController.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class HomeController {  
+    @RequestMapping("/hello")  
+    public String hello(){  
+        return"Welcome to Spring boot example!";  
+    }  
+}  
